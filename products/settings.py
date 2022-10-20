b6391170f1477b51e936c3827dbf2742f8bb4a6d
VaishnaviSunil <johndoe@example.com>
2022-10-20 13:38:07 +0530

    heroku1

--- products/settings.py
+++ products/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rcgozcdsl9g$33b=yjy7+_)eaz!n54r*rsa8ae@$xg@t*xezns
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
